378b53905a05dba3f4ee37d7105f65d6de72752a
Bruce Cui <brucec@hotmail.com>
2023-02-25 19:11:24 -0800

    got cdk to deploy succesfully

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,10 +1,9 @@
-import cdk = require('@aws-cdk/core');
-import s3deploy= require('@aws-cdk/aws-s3-deployment');
-import s3 = require('@aws-cdk/aws-s3');
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { SPADeploy } from 'cdk-spa-deploy';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new SPADeploy(this, 'websiteDeploy')
